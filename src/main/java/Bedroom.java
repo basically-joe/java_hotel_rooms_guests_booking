08f3f94c77d57159a619f624d6540a12218996f0
lacey <hiu@example.com>
2019-02-13 10:44:13 +0000

    set up bedroom class

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int roomNumber;
+    private int roomCapacity;
+    private String type;
+    private ArrayList<Guest> guests;
+
+    public Bedroom(int roomNumber, int roomCapacity, String type){
+        this.roomNumber = roomNumber;
+        this.roomCapacity = roomCapacity;
+        this.type = type;
+    }
+
+    public int getRoomNumber(){
+        return this.roomNumber;
+    }
+
+    public int getRoomCapacity(){
+        return this.roomCapacity;
+    }
+
+    public String getRoomType(){
+        return this.type;
+    }
+
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    Bedroom bedroom;
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(12, 2, "double");
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(12, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(2, bedroom.getRoomCapacity());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("double", bedroom.getRoomType());
+    }
+
 }
