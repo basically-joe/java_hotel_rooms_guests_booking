4669873293841d7019ba924188cebf7040494ee4
lacey <hiu@example.com>
2019-02-13 11:30:00 +0000

    finish MVP

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -9,7 +9,7 @@ public class Hotel {
     private ArrayList<Guest> guestsInHotel;
 
     public Hotel(String name){
-        this.name=name;
+        this.name = name;
         this.bedrooms = new ArrayList<>();
         this.conferenceRooms = new ArrayList<>();
         this.guestsInHotel = new ArrayList<>();
@@ -19,4 +19,17 @@ public class Hotel {
     public String getName(){
         return this.name;
     }
+
+    public int getGuestCount(){
+        return this.guestsInHotel.size();
+    }
+
+    public void checkInGuest(Guest guest){
+        this.guestsInHotel.add(guest);
+    }
+
+    public void checkOutGuest(){
+        this.guestsInHotel.remove(0);
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -5,15 +5,36 @@ import static org.junit.Assert.assertEquals;
 
 public class HotelTest {
     Hotel hotel;
+    Guest guest;
 
     @Before
     public void before(){
         hotel = new Hotel("Hotel Shmotel");
+        guest = new Guest();
     }
 
     @Test
     public void hasName(){
         assertEquals("Hotel Shmotel", hotel.getName());
+    }
+
+    @Test
+    public void canCheckGuestCount(){
+        assertEquals(0, hotel.getGuestCount());
+    }
+
+    @Test
+    public void canCheckInGuest(){
+        hotel.checkInGuest(guest);
+        assertEquals(1, hotel.getGuestCount());
+    }
+
+    @Test
+    public void canCheckOutGuest(){
+        hotel.checkInGuest(guest);
+        assertEquals(1, hotel.getGuestCount());
+        hotel.checkOutGuest();
+        assertEquals(0,hotel.getGuestCount());
     }
 
 }
