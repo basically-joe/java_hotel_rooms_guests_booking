cace217de111fa630f23189954a52f091a776228
lacey <hiu@example.com>
2019-02-13 11:14:25 +0000

    add dining room class and test it

--- src/main/java/DiningRoom.java
+++ src/main/java/DiningRoom.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class DiningRoom {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String typeOfFood;
+
+    public DiningRoom(String name, int capacity, String typeOfFood){
+        this.name = name;
+        this.capacity = capacity;
+        this.typeOfFood = typeOfFood;
+    }
+
+    public String getDiningRoomName(){
+        return this.name;
+    }
+
+    public int getDiningRoomCapacity(){
+        return this.capacity;
+    }
+
+    public String getTypeOfFood(){
+        return this.typeOfFood;
+    }
 }
--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiningRoomTest {
+
+    DiningRoom diningRoom;
+
+    @Before
+    public void before(){
+        diningRoom = new DiningRoom("Wednesday Room", 100, "Persian");
+    }
+
+    @Test
+    public void testHasName(){
+        assertEquals("Wednesday Room", diningRoom.getDiningRoomName());
+    }
+
+    @Test
+    public void testHasCapacity(){
+        assertEquals(100, diningRoom.getDiningRoomCapacity());
+    }
+
+    @Test
+    public void testHasTypeOfFood(){
+        assertEquals("Persian", diningRoom.getTypeOfFood());
+    }
 }
